09ae0d1aee86ccf91eef1c29c2a01769ef5e9563
Samuel Chibwe <smchibwe@gmail.com>
2023-07-13 17:10:30 +0000

    More fixes

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,5 @@
+-- Task: Aggregate fan counts per country and rank the country origins based on the total number of fans
+
 -- Create a temporary table to store the aggregated fan counts per country
 CREATE TEMPORARY TABLE temp_fan_counts AS
 SELECT origin, SUM(nb_fans) AS total_fans
@@ -7,5 +9,5 @@ GROUP BY origin;
 -- Rank the country origins based on the total number of fans
 SELECT origin, total_fans
 FROM temp_fan_counts
-ORDER BY total_fans DESC ;
+ORDER BY total_fans DESC;
 
