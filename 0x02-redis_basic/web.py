0a6dd025fe1878febe4fc53a70f2777e28d7214a
Samuel Chibwe <smchibwe@gmail.com>
2023-07-19 14:43:38 +0000

    Added Advanced task

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+web.py - A module to implement an expiring web cache and tracker.
+"""
+
+import requests
+import redis
+import time
+from typing import Callable
+
+def get_page(url: str) -> str:
+    """
+    Fetch the HTML content of the given URL and cache the result with an expiration time of 10 seconds.
+
+    Args:
+        url (str): The URL to fetch HTML content from.
+
+    Returns:
+        str: The HTML content of the given URL.
+    """
+    # Initialize Redis client
+    redis_client = redis.Redis()
+
+    # Check if the URL is already cached
+    cached_result = redis_client.get(url)
+    if cached_result:
+        print(f"Cache hit for URL: {url}")
+        return cached_result.decode("utf-8")
+
+    # If not cached, fetch the content from the URL
+    response = requests.get(url)
+
+    # Cache the response with a 10-second expiration time
+    redis_client.setex(url, 10, response.text)
+
+    # Track the number of times the URL was accessed
+    redis_client.incr(f"count:{url}")
+
+    print(f"Cache miss for URL: {url}")
+    return response.text
+
+if __name__ == "__main__":
+    # Test the get_page function
+    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
+    print(get_page(url))
+    print(get_page(url))  # Should return from cache due to the 10-second expiration
+
